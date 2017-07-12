811540b1e83a4d9a1727cfb5414eb66ca7ad4cb2
manoadamro <manoadamro.imp@gmail.com>
2017-07-12 12:34:46 +0100

    Session 1 - Challenge 7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,13 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  s = ""
+  ((0 ... string.length).find_all { |i| string.downcase[i,1] == 'r' }).each do |c|
+    if c < (string.length-1) then s += string[c+1] end
+  end
+  return s
 end
